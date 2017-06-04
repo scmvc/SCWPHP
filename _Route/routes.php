ab8feabc6914774c8575a54c3590c2487e33f56a
MichaelJions <chen@cnscbv.com>
2017-06-05 02:24:10 +0800

    version 1.0.0

--- _Route/routes.php
+++ _Route/routes.php
@@ -1,10 +1,7 @@
 <?php
 use NoahBuscher\Macaw\Macaw;
-Macaw::get("/","App\Home\Controller\IndexController@Home");
-Macaw::get("/hello",function(){
+Macaw::get("/", "App\Home\Controller\IndexController@Home");
+Macaw::get("/hello", function () {
 	print_r($_GET);
-});
-Macaw::error(function() {
-  echo '404';
 });
 Macaw::dispatch();
